82fd4e91ae1c3a9092657062f153f99fea83f99c
Junior Rodrigues <juniorpsicorodrigues@gmail.com>
2022-07-07 16:16:20 -0300

    Loja Geek

---
+++ script.js
@@ -0,0 +1,59 @@
+// // Cada item possui:
+
+// Imagem
+// Nome
+// Valor
+
+
+// DUAS SECÕES DE ITEM
+//   (quadros decorativos e bonecos de ação)
+
+// CADA ITEM É UM OBJETO E ESTA EM UMA LI
+
+
+// SEÇÃO DE BONECOS DE AÇÃO
+
+function actions(bonecosdeacao){
+
+  let yoda = {
+    name: `Boneco Yoda Star Wars`,
+    Value: `R$ 300,00`,
+  }
+  console.log(yoda)
+  let dragonball = {
+    name: `Boneco Goku Dragon Ball`,
+    Value: `R$ 250,00`,
+  }
+  console.log(dragonball)
+  let anime = {
+    name: `Boneca Asuka Langley`,
+    Value: `R$ 250,00`,
+  }
+  console.log(anime)
+}
+console.log(actions())
+
+
+
+function painting(quadrosdecorativos){
+
+    let gamepad = {
+        name: `Controle Sem Fio - Dualshock 4 Preto - Ps4`,
+        Value: `R$ 340,00`,
+    }
+    console.log(gamepad)
+    let personagem = {
+        name:`C-3PO`,
+        value:`R$ 100,00`,
+
+    }
+    console.log(personagem)
+    let clock = {
+        name:`Relógio`,
+        value:`R$ 50,00`,
+    }
+    console.log(clock)
+}
+
+console.log(painting())
+
